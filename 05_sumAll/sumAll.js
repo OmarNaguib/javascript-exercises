c3dd894058f6b889b955790969ce0877bd54c00c
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-13 03:39:34 +0200

    Solve 05 (Proud Of This Solution)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(first,second) {
+    if (typeof first !== "number" ||
+        typeof second !== "number"||
+        first<0||
+        second<0
+        ) {return "ERROR"}
 
+
+    min=Math.min(first,second)
+    n=Math.abs(first-second)+1
+
+    sum=n*(n+1)/2 + n*(min-1)
+
+    return sum
 };
 
 // Do not edit below this line
