462452bc254865c9c57f9e7f5df4fec0eed9bfd5
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-13 02:17:52 +0200

    solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(singleString,repeats) {
+    if (repeats<0) return "ERROR"
+    let fullText="";
+    for(let i=0;i<repeats;i++) {
+        fullText+=singleString;
+}
+return fullText
 };
 
 // Do not edit below this line
