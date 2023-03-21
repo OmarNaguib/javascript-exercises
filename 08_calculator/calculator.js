c8f3bce15147ea49c8a912905fe1fe904349113d
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 07:18:10 +0200

    solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
+const add = function(first,second) {
+  return first+second
 	
 };
 
-const subtract = function() {
+const subtract = function(first,second) {
+  return first-second
 	
 };
 
-const sum = function() {
+const sum = function(nums) {
+  let sum=0
+  nums.forEach(num=>{sum+=num})
+  return sum
 	
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let result=1
+  nums.forEach(num=>{result*=num})
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result=1;
+  for (let i=1;i<=number;i++) {result*=i}
+	return result
 };
 
 // Do not edit below this line
