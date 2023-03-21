025e452e0ebe1835828479dea9f4798874918c4b
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 07:01:59 +0200

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+return Math.round( ((fahrenheitTemp-32) /1.8)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  return Math.round((celTemp*1.8 +32)*10 )/10
 };
 
 // Do not edit below this line
