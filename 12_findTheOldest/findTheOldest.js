c9a552ee55d1015070ab20f397fb57746b71e094
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 22:56:34 +0200

    solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(object) {
+  object.map((person) => {
+    if (!person.yearOfDeath) { person.age = 2023 - person.yearOfBirth}
+    else {person.age = person.yearOfDeath - person.yearOfBirth}
+    return person
+  })
+
+  
+  return object.reduce((oldest,person)=> {
+      if( person.age>oldest.age  ) {return person}
+      else {return oldest}
+  })
 
 };
 
+
 // Do not edit below this line
 module.exports = findTheOldest;
+
