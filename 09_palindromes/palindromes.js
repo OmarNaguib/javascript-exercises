be419600c97c701ec0fd714ea6643ba7f3f40eff
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 07:56:32 +0200

    solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+text = text.toLowerCase().replace(/\W/g,"");
+return text === text.split("").reverse().join("")
 };
 
 // Do not edit below this line
