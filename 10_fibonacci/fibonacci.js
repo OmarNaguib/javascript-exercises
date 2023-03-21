94974fe5f5606e390f46cbc1f17ade46f9906b5e
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-21 08:02:48 +0200

    solve 09

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,8 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index<=0) return "OOPS"
+    if (index == 1) {return 1}
+    else if (index == 2) {return 1}
+    else {return fibonacci(index-1)+fibonacci(index-2)}
 
 };
 
